e433da4df43e6e7c85cc824590549af07335ff03
raphael.skawahara <raphaels.k@hotmail.com>
2023-04-03 20:46:47 -0300

    comentado algumas coisas

--- app/src/main/java/com/example/aula07/DynamicActivity.kt
+++ app/src/main/java/com/example/aula07/DynamicActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import com.example.aula07.databinding.ActivityDynamicBinding
 
 class DynamicActivity : AppCompatActivity() {
+    //binding nome precisa ser o mesmo do arquivo
     lateinit var binding: ActivityDynamicBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        //seta o binding
         binding = ActivityDynamicBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
 
         binding.buttonFrag1.setOnClickListener {//troca de fragmentos
             val fragment = FragmentTest01()
